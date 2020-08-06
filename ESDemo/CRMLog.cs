548dfb17ff8f24b3a7c69c0bdd5f469736869c83
andy99ge <zjdecjtu@163.com>
2020-08-06 18:27:10 +0800

    test code
    a c# code demo with es database for test

---
+++ ESDemo/CRMLog.cs
@@ -0,0 +1,93 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESDemo
+{
+    public class CRMLog
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string HttpMethod { get; set; }
+        /// <summary>
+        /// 请求地址
+        /// </summary>
+        public string RequestUrl { get; set; }
+        /// <summary>
+        /// 请求Json
+        /// </summary>
+        public string RequestJson { get; set; }
+        /// <summary>
+        /// 相应Json
+        /// </summary>
+        public string ResponseJson { get; set; }
+
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public string ProjectName { get; set; }
+        /// <summary>
+        /// 控制器
+        /// </summary>
+        public string Controller { get; set; }
+
+        /// <summary>
+        /// 方法
+        /// </summary>
+        public string Action { get; set; }
+
+        /// <summary>
+        /// ip地址
+        /// </summary>
+        public string IPAddress { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// 内容体
+        /// </summary>
+        public string Content { get; set; }
+
+
+        /// <summary>
+        /// 方法执行时间
+        /// </summary>
+        public long ActionTime { get; set; }
+
+        /// <summary>
+        /// 异常等级
+        /// </summary>
+        public int LogLevel { get; set; }
+
+        /// <summary>
+        /// 用户工号
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// 用户姓名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 日志类型
+        /// </summary>
+        public string LogType { set; get; }
+        /// <summary>
+        /// 唯一值
+        /// </summary>
+        public string OnlyKey { get; set; }
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        public string ErrorInfo { get; set; }
+        /// <summary>
+        /// 浏览器版本
+        /// </summary>
+        public string UserAgent { get; set; }
+    }
+}
---
+++ ESDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Elasticsearch.Net;
+using Elasticsearch.Net.Diagnostics;
+using Nest;
+using Newtonsoft.Json;
+
+namespace ESDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = new Uri[]
+            {
+                new Uri("http://localhost:9200")
+            };
+            var pool = new StaticConnectionPool(nodes);
+
+            var settings = new ConnectionSettings(pool).DefaultIndex("crm.base.log");
+
+            var client = new ElasticClient(settings);
+
+            //for (int i = 200; i < 500; i++)
+            //{
+            //    var log = new CreateRequest<CRMLog>(Guid.NewGuid());
+            //    CRMLog tempDoc = new CRMLog();
+            //    tempDoc.LogLevel = i;
+            //    tempDoc.HttpMethod = "POST";
+            //    tempDoc.OnlyKey = Guid.NewGuid().ToString("N");
+            //    tempDoc.IPAddress = "0.0.0.0";
+            //    log.Document = tempDoc;
+            //    client.Create<CRMLog>(log);
+            //}
+
+
+            //  client.Delete(new DeleteRequest("crm.base.log", "4bd0ffb9-f5fc-4748-94f6-b41c706cc4ca"));
+            //    var result=  client.Delete<CRMLog>(new DocumentPath<CRMLog>("e949fcc6-4ada-4643-b8cd-c6d4d52cced2"));
+
+
+
+            //var bulkDel = new BulkRequest() { Operations = new List<IBulkOperation>() };
+            //bulkDel.Operations.Add(new BulkDeleteOperation<CRMLog>("4bd0ffb9-f5fc-4748-94f6-b41c706cc4ca"));
+            //bulkDel.Operations.Add(new BulkDeleteOperation<CRMLog>("aca1f759-43ec-4c2c-b9ce-fe0f6c644a48"));
+            //var resultDel = client.Bulk(bulkDel);
+
+
+
+            //IUpdateRequest<CRMLog, CRMLog> request = new UpdateRequest<CRMLog, CRMLog>("7d8ebbcc-b80a-4104-a974-519474563fc2")
+            //{
+            //    Doc = new CRMLog()
+            //    {
+            //        LogLevel = 6,
+            //        HttpMethod = "test4update........"
+            //    }
+            //};
+            //var resp = client.Update<CRMLog, CRMLog>(request);
+
+
+            //var bulkUpdate = new BulkRequest() { Operations = new List<IBulkOperation>() };
+            //bulkUpdate.Operations.Add(new BulkUpdateOperation<CRMLog, CRMLog>("6cfb5050-d175-4fb1-8f8d-c6d88cb0e2a4") { Doc = new CRMLog() { HttpMethod = "xiugai" } });
+            //bulkUpdate.Operations.Add(new BulkUpdateOperation<CRMLog, CRMLog>("aca1f759-43ec-4c2c-b9ce-fe0f6c644a48") { Doc = new CRMLog() { HttpMethod = "xiugai1" } });
+            //var result = client.Bulk(bulkUpdate);
+
+
+            //var modUser = client.Get<CRMLog>("ef6c9de4-196e-46b1-bfd4-ecfcae5ecb32");
+            //var tweet = JsonConvert.SerializeObject(modUser.Source);
+            //Console.WriteLine(tweet);
+
+
+            //var ts = client.Search<CRMLog>(s => s
+            //.From(0)
+            //.Size(1000)
+            //.Query(q =>
+            //          q.Match(m => m.Field(f => f.LogLevel).Query("318")))).Documents;
+
+            //foreach (var t in ts)
+            //{
+            //    Console.WriteLine("LogLevel:{0},HttpMethod:{1}", t.LogLevel, t.HttpMethod);
+            //}
+
+
+            // client.Search<CRMLog>.(s=>s.Query(q => q.Match(m => m.Field(f => f.Type).Query(type)))).Documents;
+
+
+            // var modList = client.Search<CRMLog>(s => s
+            //.From(0)
+            //.Size(10)
+            //.Query(q =>
+            //        q.Term(t => t.HttpMethod, "www.b.com")
+            //        || q.Match(mq => mq.Field(f => f.HttpMethod).Query("Get"))
+            //    )
+            // );
+            // var tweet = JsonConvert.SerializeObject(modList);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
